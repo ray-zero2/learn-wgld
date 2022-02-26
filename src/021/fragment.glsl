dbe2ee3eea0931451cdaf81123686b9e10439f36
ray-zero2 <rei2219.matsuda@gmail.com>
2022-02-26 20:52:53 +0900

    change to phong shading

--- src/021/fragment.glsl
+++ src/021/fragment.glsl
@@ -1,5 +1,17 @@
-precision mediump float;
+precision highp float;
+uniform   vec4 ambientColor;
+uniform   mat4 modelMatrix;
+uniform   mat4 invMatrix;
+uniform   vec3 eyeDirection;
+uniform   vec3 lightDirection;
+varying vec3 vNormal;
 varying vec4 vColor;
 void main(void){
-	gl_FragColor = vColor;
+	vec3 light = normalize(lightDirection);
+	vec4 mNormal = normalize(modelMatrix * vec4(vNormal, 0.));
+	vec3 halfLE = normalize(lightDirection + eyeDirection);
+	float diffuse  = clamp(dot(mNormal.xyz, light), .0, 1.0);
+	float specular = pow(clamp(dot(mNormal.xyz, halfLE), .0, 1.0), 50.);
+	vec4 finalColor = vColor * vec4(vec3(diffuse), 1.0) + vec4(vec3(specular), 1.0) + ambientColor;
+	gl_FragColor = finalColor;
 }
--- src/021/vertex.glsl
+++ src/021/vertex.glsl
@@ -1,23 +1,15 @@
 attribute vec3 position;
 attribute vec3 normals;
 attribute vec4 color;
-uniform   vec4 ambientColor;
 uniform   mat4 modelMatrix;
 uniform   mat4 viewMatrix;
 uniform   mat4 projectionMatrix;
-uniform   mat4 invMatrix;
-uniform   vec3 eyeDirection;
-uniform   vec3 lightDirection;
+varying vec3 vNormal;
 varying   vec4 vColor;
 
 void main(void){
-    // vec3  invLight = normalize(invMatrix * vec4(lightDirection, 0.0)).xyz;
-		vec3 light = normalize(lightDirection);
 		vec4 mPosition = modelMatrix * vec4(position, 1.);
-		vec4 mNormal = normalize(modelMatrix * vec4(normals, 0.));
-    vec3 halfLE = normalize(lightDirection + eyeDirection);
-    float diffuse  = clamp(dot(mNormal.xyz, light), .0, 1.0);
-    float specular = pow(clamp(dot(mNormal.xyz, halfLE), .0, 1.0), 50.);
-    vColor         = color * vec4(vec3(diffuse), 1.0) + vec4(vec3(specular), 1.0) + ambientColor;
+    vColor = color;
+    vNormal = normals;
     gl_Position    = projectionMatrix * viewMatrix * mPosition;
 }
